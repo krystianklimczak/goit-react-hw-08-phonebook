2f152cd7af72a2da413677061040f9ec1d325976
krystianklimczak <krystianklimczak1996r.com>
2023-10-23 19:36:36 +0200

    add all components to app and add logic, styles & proptypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,43 +7,30 @@ import Section from './sections/Section'
 import Contacts from './contacts/Contacts'
 import Filter from './filter/Filter'
 
-// library to generete unique id
+// external libraries
 import { nanoid } from 'nanoid'
+import Notiflix from 'notiflix'
 
 /// css modules
-// import css from './App.module.css';
-
-// initial values of app state
-const initialValues = {
-  name: '',
-  number: '',
-  filter: '',
-}
+import css from './App.module.css';
 
 export default class App extends Component {
-  // set start state
-  state = {...initialValues, contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]};
+
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: ''
+  };
 
   // change value of input handler
   handlerChange = ev => {
     const {name, value} = ev.target;
     this.setState({ [name]: value});
   }
-
-  // handlerFilter = ev => {
-  //   const {name, value} = ev.target;
-  //   this.setState({ [name]: value });
-  //   const prevState = this.state;
-  //   console.log(prevState);
-  //   this.setState({
-  //     contacts: [...(prevState.contacts.filter(contact => contact.name.toUpperCase().includes(value.toUpperCase())))]
-  //   })
-  // }
 
   // submit form handler
   handlerSubmit = ev => {
@@ -53,41 +40,58 @@ export default class App extends Component {
     const name = form.elements.name.value;
     const number = form.elements.number.value;
 
+    // check if there is contact with that name / if yes, return 
+    const allContacts = this.state.contacts;
+    if (allContacts.find(contact => contact.name.toUpperCase() === name.toUpperCase())) {
+      return Notiflix.Notify.failure(`Contact ${name} already exist on list`)
+    }
+
+    // generate new contact
     const newContact = {
       name: name,
       number: number,
       id: nanoid(),
     }
 
+    // update contacts list state
     this.setState({
-      contacts: [...this.state.contacts, newContact]
+      contacts: [...this.state.contacts, newContact],
+      filter: ''
     })
 
-    this.reset();
+    // reset form inputs
+    form.reset();
   }
 
-  // reset form handler
-  reset = () => {
-    this.setState(initialValues);
+  // delete contact handler
+  handlerDelete = (ev) => {
+    const deletedContactId = ev.target.value;
+    const updatedContacts = this.state.contacts.filter(contact => !(contact.id === deletedContactId));
+
+    this.setState({
+      contacts: [...updatedContacts]
+    })
   }
 
   // render
   render() {
     // destructuring assignment
     const {name, number, filter, contacts} = this.state;
+
+    // declatarion of filtered contacts of user input
     const filteredContacts = contacts.filter(contact => contact.name.toUpperCase().includes(filter.toUpperCase()));
 
     return (
-      <div>
-        <h1>Phonebook</h1>
+      <div className={css.container}>
+        <h1 className={css.phoneBook}>Phonebook</h1>
 
         <Section title="Add Contact">
-          <Form name={name} number={number} onChange={this.handlerChange} onSubmit={this.handlerSubmit}/>
+          <Form name={name} number={number} onSubmit={this.handlerSubmit}/>
         </Section>
         
         <Section title="Contacts">
-          <Contacts contacts={filteredContacts}>
-            <Filter filter={filter} onChange={this.handlerChange}/>
+          <Contacts contacts={filteredContacts} handlerRemove={this.handlerDelete}>
+            {contacts.length > 0 && <Filter filter={filter} onChange={this.handlerChange}/>}
           </Contacts>
         </Section>
       </div>
