fe19d09b5fe3de84a7a32dfb3a64a2c34600d65b
krystianklimczak <krystianklimczak1996r.com>
2023-10-29 05:47:33 +0100

    new comments styles by block from shortest to longest

--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -1,28 +1,22 @@
-// 'React.Component'
-import React, { Component } from 'react'
-
-// css modules
-import css from './Section.module.css';
-
-// proptypes
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import css from './Section.module.css';
 
 export default class Section extends Component {
   render() {
     const { title, children } = this.props;
-    
+
     return (
       <div className={css.section}>
         <h2 className={css.section__title}>{title}</h2>
-        <div>
-            {children}
-        </div>
+        <div>{children}</div>
       </div>
-    )
+    );
   }
 }
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
-}
+};
