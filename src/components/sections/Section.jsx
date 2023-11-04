d7134ecfe80fe95262f6f71ebeb7da3362908f40
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 16:56:17 +0100

    change Section class component to function component, format imports and add propTypes

--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -1,19 +1,14 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
 import css from './Section.module.css';
 
-export default class Section extends Component {
-  render() {
-    const { title, children } = this.props;
-
-    return (
-      <div className={css.section}>
-        <h2 className={css.section__title}>{title}</h2>
-        <div>{children}</div>
-      </div>
-    );
-  }
+export default function Section({ title, children }) {
+  return (
+    <div className={css.section}>
+      <h2 className={css.section__title}>{title}</h2>
+      <div>{children}</div>
+    </div>
+  );
 }
 
 Section.propTypes = {
