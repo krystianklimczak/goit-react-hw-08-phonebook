0e4f880b0b50aee80f46a363c6b3258a974417c3
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 16:55:50 +0100

    change Reset class component to function component & format imports

--- src/components/reset/Reset.jsx
+++ src/components/reset/Reset.jsx
@@ -1,16 +1,15 @@
-// 'React.Component'
-import React, { Component } from 'react'
+import { useContacts } from 'hooks/ContactContext';
 
-/// css modules
 import css from './Reset.module.css';
 
-export default class Reset extends Component {
-  render() {
-    const { onClick } = this.props;
-    return (
-      <div className={css.reset}>
-        <button className={css.reset__btn} type='button' onClick={onClick}>Reset</button>
-      </div>
-    )
-  }
+export default function Reset() {
+  const { handleReset } = useContacts();
+
+  return (
+    <div className={css.reset}>
+      <button className={css.reset__btn} type="button" onClick={handleReset}>
+        Reset
+      </button>
+    </div>
+  );
 }
