d4ed993849ac54d3822781d6cb6a961b7a1cc32b
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 16:55:30 +0100

    change Form class component to function component & format imports

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,47 +1,35 @@
-// 'React.Component'
-import React, { Component } from 'react'
+import { useContacts } from 'hooks/ContactContext';
 
-// css modules
 import css from './Form.module.css';
 
-// proptypes
-import PropTypes from 'prop-types';
+export default function Form() {
+  const { handleSubmit } = useContacts();
 
-export default class Form extends Component {
-  render() {
-    const { onSubmit } = this.props;
-    return (
-      <form onSubmit={onSubmit} className={css.form}>
-        <label className={css.label}>
-            Name
-            <input
-                type="text"
-                name="name"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                className={css.input}
-            />
-        </label>
-        <label className={css.label}>
-            Number
-            <input
-                type="tel"
-                name="number"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                className={css.input}
-            />
-        </label>
-        <button type='submit' className={css.btn}>
-            Add contact
-        </button>
-      </form>
-    )
-  }
-}
-
-Form.propTypes = {
-  name: PropTypes.string,
-  number: PropTypes.string, 
-  onSubmit: PropTypes.func.isRequired
+  return (
+    <form onSubmit={handleSubmit} className={css.form}>
+      <label className={css.label}>
+        Name
+        <input
+          type="text"
+          name="name"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          className={css.input}
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          type="tel"
+          name="number"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          className={css.input}
+        />
+      </label>
+      <button type="submit" className={css.btn}>
+        Add contact
+      </button>
+    </form>
+  );
 }
