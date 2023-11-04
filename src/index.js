5de18007927ff53a688c357fb124295ce3584db1
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 16:54:03 +0100

    add context provider for app & format imports

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from 'components/App';
+import { ContactsProvider } from 'hooks/ContactContext';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContactsProvider>
+      <App />
+    </ContactsProvider>
   </React.StrictMode>
 );
