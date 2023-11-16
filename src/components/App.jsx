8446aabd3d5f93c19b32e1091149ecabb9439ab0
krystianklimczak <krystianklimczak1996r.com>
2023-11-16 22:18:15 +0100

    refactor component with react redux store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,42 @@ import Reset from './reset/Reset';
 import Filter from './filter/Filter';
 import Section from './sections/Section';
 import Contacts from './contacts/Contacts';
-import { useContacts } from 'hooks/ContactContext';
+
+import { useEffect } from 'react';
+import { initialState } from 'redux/contactSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { localStorageCheck } from 'redux/contactSlice';
 
 import css from './App.module.css';
 
 export default function App() {
-  const { contacts } = useContacts();
+  const contacts = useSelector(state => state.contacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const json = localStorage.getItem('contacts');
+
+    if (JSON.parse(json) === null) {
+      const json = JSON.stringify(initialState.contacts);
+      localStorage.setItem('contacts', json);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = localStorage.getItem('contacts');
+    const localContacts = JSON.parse(json);
+
+    if (JSON.parse(json) !== null) {
+      dispatch(localStorageCheck(localContacts));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (contacts !== initialState.contacts) {
+      const json = JSON.stringify(contacts);
+      localStorage.setItem('contacts', json);
+    }
+  }, [contacts]);
 
   return (
     <div className={css.container}>
