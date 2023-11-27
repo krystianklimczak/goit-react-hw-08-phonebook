fe5ec49835e13eca07c0dd5e785591f9785309cb
krystianklimczak <krystianklimczak1996r.com>
2023-11-27 18:38:25 +0100

    deleted necesary com

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -28,36 +28,32 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      // .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items = action.payload;
       })
-      // .addCase(fetchContacts.rejected, handleRejected)
-      // .addCase(addContact.pending, handlePending)
+
       .addCase(addContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         state.items.push(action.payload);
       })
-      // .addCase(addContact.rejected, handleRejected)
-      // .addCase(deleteContact.pending, handlePending)
+
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         const index = state.items.findIndex(contact => contact.id === action.payload.id);
         state.items.splice(index, 1);
       })
-      // .addCase(deleteContact.rejected, handleRejected)
-      // .addCase(toggleBlocked.pending, handlePending)
+
       .addCase(toggleBlocked.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         const index = state.items.findIndex(contact => contact.id === action.payload.id);
         state.items[index].blocked = !state.items[index].blocked;
       })
-      // .addCase(toggleBlocked.rejected, handleRejected);
+
       .addMatcher(isPendingAction, handlePending)
       .addMatcher(isRejectAction, handleRejected)
       .addDefaultCase((state, action) => {
