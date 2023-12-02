242902399a47bdcdbe26edeb33920dfd39597b46
krystianklimczak <krystianklimczak1996r.com>
2023-12-02 17:57:30 +0100

    update page404 button (still working on)

--- src/components/page404/Page404.jsx
+++ src/components/page404/Page404.jsx
@@ -1,13 +1,13 @@
+import { Button } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Page404() {
   return (
     <div>
-      Page404
-      <button>
-        <Link to="/goit-react-hw-08-phonebook">Go back to main page</Link>
-      </button>
+      <Link to="/goit-react-hw-08-phonebook">
+        <Button variant="contained">Go back to main page</Button>
+      </Link>
     </div>
   );
 }
